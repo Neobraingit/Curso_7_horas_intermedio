f4059dbdcc78c818a5b651c5105553b92311978a
Neobraingit <Neobraingit@github.com>
2022-11-26 16:47:38 +0100

    Manejo de ficheros

---
+++ 05_Tipos_de_error.py
@@ -0,0 +1,35 @@
+# Tipos de error
+
+# SintaxError
+# print 'Hola mundo' --- Esto nos provoca un error de sintaxis al faltar los paréntesis
+
+# NameError
+# print (name) --- La variable no está definida y dará error
+
+# IndexError
+# my_list = ['Python', 'Swift', 'Javascript']
+# print (my_list [5]) --- Esto dará error porque el índice 5 no existe
+
+# ModuleNotFoundError
+# import maths --- Esto dará error porque el módulo no existe
+
+# AttibruteError
+# print (math.PI) --- Esto dará error porque el atributo está mal escrito
+
+# KeyError
+dict = {'Nombre' : 'Marcos', 'Apellido' : 'Carmona'}
+# print (dict['Nobre']) --- Esto dará error porque esa clave no existe
+
+# TypeError
+# print (my_list['Nombre']) --- Esto dará error porque los índices deben ser integers al hacer slicing
+
+# ImportError
+# from math import pii --- Esto dará error porque pi está mal escrito y no existe
+
+# ValueError
+# my_int = int(10_años) --- Esto dará error porque la palabra años no puede transformarse en entero
+
+# ZeroDivisionError
+# print(4 / 0) --- No se puede hacer la división
+
+
